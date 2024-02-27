a6e472d970e5846c6c3991fa2dc4ca5f87aa500d
Manoj <manojkumar.srirapu@gmail.com>
2024-02-27 08:28:01 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import NotFound from "./components/NotFound"
 
 const App = () => (
    <Switch>
-    <Route exact path="/signup" component={SignUpForm} />
-    <Route exact path = "/" component = {Home} />
-    <Route exact path = "/not-found" component = {NotFound} />
-    <Redirect to = "/not-found" />
+      <Route exact path="/signup" component={SignUpForm} />
+      <Route exact path = "/" component = {Home} />
+      <Route exact path = "/not-found" component = {NotFound} />
+      <Redirect to = "/not-found" />
    </Switch>
 )
 
